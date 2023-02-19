11bd95e821a76b2015bf2f7117d1ae4f4b3b7baa
Amirkhan <amirhan75@gmail.com>
2023-02-19 15:12:20 +0600

    hm9

---
+++ 3/Program.cs
@@ -0,0 +1,13 @@
+﻿// 
+        public override void Execute()
+        {
+            var m = Utility.ReadIntConsole("Введите число m");
+            var n = Utility.ReadIntConsole("Введите число n");
+
+            Console.WriteLine($"Функция Аккермана: {Utility.AckermannFunction(m, n)}");
+        }
+
+        public override string GetName()
+        {
+            return "Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.";
+        }
